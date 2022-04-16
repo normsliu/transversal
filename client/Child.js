4ff5e0ac5eb6cfbe3f9ab2231a2533d8e945ede3
han900204 <54014070+han900204@users.noreply.github.com>
2022-04-16 14:44:01 -0700

    updates

--- client/Child.js
+++ client/Child.js
@@ -3,35 +3,83 @@ import React, { useState, useEffect } from 'react';
 const Child = ({ trans }) => {
 	const [defaultUsers, setDefaultUsers] = useState();
 	const [customUsers, setCustomUsers] = useState();
+	const transversalQuery = async (
+		gql,
+		variables,
+		cacheOption = false,
+		custom
+	) => {
+		if (custom) {
+			const pattern = /^.+{$/gm;
+
+			const queryString = pattern.exec(gql);
+			const queryString2 = pattern.exec(gql);
+
+			gql =
+				queryString +
+				'\n' +
+				queryString2 +
+				'\n' +
+				custom +
+				'\n' +
+				'}' +
+				'\n' +
+				'}';
+		}
+
+		const request = async (endpoint, gql, variables) => {
+			const res = await fetch(endpoint, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+				},
+				body: JSON.stringify({
+					query: gql,
+					variables: variables,
+				}),
+			})
+				.then((res) => res.json())
+				.then((data) => data);
+			return res;
+		};
+
+		if (!cacheOption) {
+			console.log('caching option not selected');
+			const res = await request('/graphql', gql, variables);
+			return res;
+		} else {
+			console.log('caching option selected!');
+
+			const res = await fetch('/transversal', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+				},
+				body: JSON.stringify({
+					query: gql,
+					variables: variables,
+				}),
+			})
+				.then((res) => res.json())
+				.then((data) => data);
+			return res;
+		}
+	};
 
 	useEffect(() => {
 		const query = async () => {
-			console.log('mounting', trans.transversalQuery);
-			// const res = await fetch('/graphql', {
-			// 	method: 'POST',
-			// 	headers: {
-			// 		'Content-Type': 'application/json',
-			// 		Accept: 'application/json',
-			// 	},
-			// 	body: JSON.stringify({
-			// 		query: trans.gql.getUsers,
-			// 		variables: { age: 10, height: 10 },
-			// 	}),
-			// })
-			// 	.then((res) => res.json())
-			// 	.then((data) => data);
-			// console.log('done?', res);
-			// return res;
+			console.log('checking query', trans.transversalQuery);
 
-			const users = await trans.transversalQuery(
+			const users = await transversalQuery(
 				trans.gql.getUsers,
 				{
 					age: 10,
 					height: 10,
 				},
-				true
+				false
 			);
-			console.log('users', users);
 			setDefaultUsers(users);
 
 			// const customUsers = await trans.transversalQuery(
@@ -50,7 +98,7 @@ const Child = ({ trans }) => {
 		};
 		query();
 	}, [trans]);
-
+	console.log('Default users', defaultUsers);
 	return <h1>Hello World!</h1>;
 };
 
