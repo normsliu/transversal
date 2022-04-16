a768cfc7b96deb748b801b688c18948fa5ff47de
han900204 <54014070+han900204@users.noreply.github.com>
2022-04-16 14:37:55 -0700

    updates

--- client/Child.js
+++ client/Child.js
@@ -5,43 +5,50 @@ const Child = ({ trans }) => {
 	const [customUsers, setCustomUsers] = useState();
 
 	useEffect(() => {
-		console.log('mounting', trans.transversalQuery);
-		const users = trans.transversalQuery(
-			trans.gql.getUsers,
-			{
-				age: 10,
-				height: 10,
-			},
-			false
-		);
-		console.log('users', users);
-		// const query = async () => {
-		// 	const users = await trans.transversalQuery(
-		// 		trans.gql.getUsers,
-		// 		{
-		// 			age: 10,
-		// 			height: 10,
-		// 		},
-		// 		false
-		// 	);
-		// 	console.log('users', users);
-		// 	setDefaultUsers(users);
+		const query = async () => {
+			console.log('mounting', trans.transversalQuery);
+			// const res = await fetch('/graphql', {
+			// 	method: 'POST',
+			// 	headers: {
+			// 		'Content-Type': 'application/json',
+			// 		Accept: 'application/json',
+			// 	},
+			// 	body: JSON.stringify({
+			// 		query: trans.gql.getUsers,
+			// 		variables: { age: 10, height: 10 },
+			// 	}),
+			// })
+			// 	.then((res) => res.json())
+			// 	.then((data) => data);
+			// console.log('done?', res);
+			// return res;
 
-		// 	const customUsers = await trans.transversalQuery(
-		// 		trans.gql.getCustom,
-		// 		{
-		// 			age: 10,
-		// 			height: 10,
-		// 		},
-		// 		false,
-		// 		`firstName
-		// 		lastName
-		// 		age
-		// 		height`
-		// 	);
-		// 	setCustomUsers(customUsers);
-		// };
-		// query();
+			const users = await trans.transversalQuery(
+				trans.gql.getUsers,
+				{
+					age: 10,
+					height: 10,
+				},
+				true
+			);
+			console.log('users', users);
+			setDefaultUsers(users);
+
+			// const customUsers = await trans.transversalQuery(
+			// 	trans.gql.getCustom,
+			// 	{
+			// 		age: 10,
+			// 		height: 10,
+			// 	},
+			// 	false,
+			// 	`firstName
+			// 	lastName
+			// 	age
+			// 	height`
+			// );
+			// setCustomUsers(customUsers);
+		};
+		query();
 	}, [trans]);
 
 	return <h1>Hello World!</h1>;
